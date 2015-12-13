fd3035afa6d8a04b84002be194d2ecf9a6d1cf00
Alex Kale <hi@awkale.me>
2015-12-13 15:24:39 -0500

    set the wrapper with programmatically

--- javascript/main.js
+++ javascript/main.js
@@ -10,12 +10,6 @@
 
   var $slideWrapper = document.querySelector('.slideshow__wrapper');
 
-  // get number of images
-  // to get contraint and reset
-
-  var $imgs = document.querySelectorAll('.slideshow__img');
-  var imgLen = $imgs.length;
-
   // width of each image / wrapper
   // to know how far to move
 
@@ -24,12 +18,19 @@
   var width = slideshowItemStyles.width;
   width = parseInt( width, 10);
 
+  // get number of images
+  // to get contraint and reset
+  var slideLen = document.querySelectorAll('.slideshow__item').length;
+
   // keep track of current image
-  var currImage = 0
+  var currImage = 0;
+
+  // update width of slideshow wrapper
+  $slideWrapper.style.width = slideLen * width + 'px';
 
   function onInterval() {
     currImage++;
-    if ( currImage === imgLen ) {
+    if ( currImage === slideLen ) {
       currImage = 0;
     }
     $slideWrapper.style.left = -1*currImage*width+'px';
