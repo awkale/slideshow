01a654865e04e3b6ee5e370480fdd29f3c787f1f
Alex Kale <hi@awkale.me>
2015-12-13 14:33:37 -0500

    auto create wrapper div

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,13 @@
 /* CURRENTLY IN: javascript/main.js */
+(function() { // protect
+  // your goal:
+  // using javascript, wrap the contents
+  // of the $slideshow var
+  // with:
+  // <div class="slideshow__wrapper clearfix">..image divs here..</div>
+  var $slideshow = document.querySelector('.slideshow'); // $(".slideshow")
+  $slideshow.innerHTML = '<div class="slideshow__wrapper cf">' + $slideshow.innerHTML + '</div>';
+
+
+
+})();
