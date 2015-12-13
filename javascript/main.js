50940532bc48e8e67d4a1f5b609ebea0c917db49
Alex Kale <hi@awkale.me>
2015-12-13 15:45:44 -0500

    refactor to have multiple sliders on page

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,53 @@
 /* CURRENTLY IN: javascript/main.js */
 (function() { // protect
-  // your goal:
-  // using javascript, wrap the contents
-  // of the $slideshow var
-  // with:
-  // <div class="slideshow__wrapper clearfix">..image divs here..</div>
-  var $slideshow = document.querySelector('.slideshow'); // $(".slideshow")
-  $slideshow.innerHTML = '<div class="slideshow__wrapper cf">' + $slideshow.innerHTML + '</div>';
 
-  var $slideWrapper = document.querySelector('.slideshow__wrapper');
+  function makeSlider( slideClass ) {
+    // your goal:
+    // using javascript, wrap the contents
+    // of the $slideshow var
+    // with:
+    // <div class="slideshow__wrapper clearfix">..image divs here..</div>
+    var $slideshow = document.querySelector(slideClass); // $(".slideshow")
 
-  // width of each image / wrapper
-  // to know how far to move
+    $slideshow.innerHTML = '<div class="slideshow__wrapper cf">' + $slideshow.innerHTML + '</div>';
 
-  var $slideshowItem = document.querySelector('.slideshow__item');
-  var slideshowItemStyles = getComputedStyle( $slideshowItem );
-  var width = slideshowItemStyles.width;
-  width = parseInt( width, 10);
+    var $slideWrapper = document.querySelector(slideClass + ' .slideshow__wrapper');
 
-  // get number of images
-  // to get contraint and reset
-  var slideLen = document.querySelectorAll('.slideshow__item').length;
+    // width of each image / wrapper
+    // to know how far to move
 
-  // keep track of current image
-  var currImage = 0;
+    var $slideshowItem = document.querySelector(slideClass + ' .slideshow__item');
+    var slideshowItemStyles = getComputedStyle( $slideshowItem );
+    var width = slideshowItemStyles.width;
+    width = parseInt( width, 10);
 
-  // update width of slideshow wrapper
-  $slideWrapper.style.width = slideLen * width + 'px';
+    // get number of images
+    // to get contraint and reset
+    var slideLen = document.querySelectorAll(slideClass + ' .slideshow__item').length;
 
-  function onInterval() {
-    currImage++;
-    if ( currImage === slideLen ) {
-      currImage = 0;
+    // keep track of current image
+    var currImage = 0;
+
+    // update width of slideshow wrapper
+    $slideWrapper.style.width = slideLen * width + 'px';
+
+    function onInterval() {
+      currImage++;
+      if ( currImage === slideLen ) {
+        currImage = 0;
+      }
+      $slideWrapper.style.left = -1*currImage*width+'px';
     }
-    $slideWrapper.style.left = -1*currImage*width+'px';
-  }
-  setInterval(onInterval, 1000);
+    setInterval(onInterval, 1000);
+  } // makeSlider
 
+  function initSlideshows( slideClass) {
+    var slideshows = document.querySelectorAll(slideClass);
+    for ( var i = 0; i < slideshows.length; ++i) {
+      slideshows[ i ].classList.add('slideshow-'+i);
+      makeSlider('.slideshow-'+i);
+    }
+  }
+
+  initSlideshows('.slideshow');
 })();
